f4203a894138a515f448e2d53df38e262b72847a
BahaaAldein <BahaaAldein@gmail.com>
2024-03-07 09:41:49 +0200

    first commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhook/clerk", "/api/webhook/stripe"],
+  ignoredRoutes: ["/", "/api/webhook/clerk", "/api/webhook/stripe"],
 });
 
 export const config = {
